ad69729524ca2d0c7525f214a3787429411a7e83
altair961 <altair89@list.ru>
2026-01-10 23:39:28 +0300

    Java classpath update. Comments removal

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -8,21 +8,8 @@ import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 
-/**
- * This class contains the main method and is used to test the engine.
- * 
- * @author Karl
- *
- */
 public class MainGameLoop {
 
-
-	/**
-	 * Loads up the position data for two triangles (which together make a quad)
-	 * into a VAO. This VAO is then rendered to the screen every frame.
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		
 		DisplayManager.createDisplay();
